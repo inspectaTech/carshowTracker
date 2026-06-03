097e56de48c88ef7ccc1f130637f3b9ca3fd650d
inspectaTech <d3po.techengineer@gmail.com>
2026-06-03 17:38:05 -0400

    deploy: add start script, clean vite config, update for production

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -13,4 +11,4 @@ export default defineConfig({
     tailwindcss(),
     viteReact(),
   ],
-})
+})
